8209ef0363249d57f9d8013e45c5f6ec58f97717
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-07-11 11:28:54 +0900

    fix #1

--- cleanup.sh
+++ cleanup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Remove Linux headers
-yum -y remove kernel-devel kernel-headers
+yum -y remove gcc kernel-devel kernel-headers
 yum -y clean all
 
 # Remove Virtualbox specific files
 rm -rf /usr/src/vboxguest* /usr/src/virtualbox-ose-guest*
-# rm -rf *.iso *.iso.? /tmp/vbox /home/vagrant/.vbox_version
+rm -rf *.iso *.iso.? /tmp/vbox /home/vagrant/.vbox_version
 
 # Cleanup log files
 find /var/log -type f | while read f; do echo -ne '' > $f; done;
--- virtualbox.sh
+++ virtualbox.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-yum install -y patch
-
-mkdir /tmp/vbox
-mount -t iso9660 -o loop /home/vagrant/VBoxGuestAdditions.iso /tmp/vbox
-/tmp/vbox/VBoxLinuxAdditions.run
-umount /tmp/vbox
+mount -o loop /home/vagrant/VBoxGuestAdditions.iso /mnt
+/mnt/VBoxLinuxAdditions.run
+umount /mnt
+rm -rf VBoxGuestAdditions.iso
