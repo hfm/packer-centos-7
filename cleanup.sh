18de02efe6d640f7c6720dd2dbf07496d841a5c8
Takahiro OKUMURA <hfm.garden@gmail.com>
2014-07-19 01:00:47 +0900

    Remove UUID and HWADDR from ifcfg-enp0s3

--- cleanup.sh
+++ cleanup.sh
@@ -15,6 +15,8 @@ find /var/log -type f | while read f; do echo -ne '' > $f; done;
 rm -rf /tmp/*
 
 rm -f /etc/udev/rules.d/70-persistent-net.rules
+sed -i '/^UUID/d'   /etc/sysconfig/network-scripts/ifcfg-enp0s3
+sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-enp0s3
 
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -rf /EMPTY
