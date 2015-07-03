da037ce7e5872836f0d41fdfb1c46d1a23242b90
Takahiro OKUMURA <hfm.garden@gmail.com>
2015-07-03 15:56:34 +0900

    diet

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Remove Linux headers
-yum -y remove gcc kernel-devel kernel-headers
+yum -y remove gcc kernel-devel kernel-headers perl cpp
 yum -y clean all
 
 # Remove Virtualbox specific files
